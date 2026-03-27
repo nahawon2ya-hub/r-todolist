b5e68833bbba5d6c7bd36e7da081a4f92486aedf
hyewon <nahawon2ya@gmail.com>
2026-03-27 09:34:47 +0900

    dfa

--- front_/src/comp/TodoHead.jsx
+++ front_/src/comp/TodoHead.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import todoStore from '../store/TodoStore'
 
 function TodoHead() {
-  const {get} = todoStore()
+  const {get, data} = todoStore()
+
+  let done =  data.filter(item=>item.isdone === true)
+  
+  // data.filter(function(item) { 
+  //   return item.isdone === true
+  // })
   
   return (
     <div className='head'>
         <h2>TodoList</h2>
         <div>
-            <div>할일(6) | 완료(2)</div>
+            <div>할일({data.length}) | 완료({done.length})</div>
             <div className='btns'>
                 <button onClick={e=>get('all')}>전체</button>
                 <button onClick={e=>get(false)}>진행중</button>
